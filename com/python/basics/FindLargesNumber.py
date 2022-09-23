55ad12a2e844e88b4130f0eba7e82e0d8c2dacc4
Vibinson <vibinvictoria@gmail.com>
2022-09-23 14:50:16 +0530

    Find Largest Number - Initial Version

--- com/python/basics/FindLargesNumber.py
+++ com/python/basics/FindLargesNumber.py
@@ -3,7 +3,30 @@
     [1,2,3,4,5,6] => 6
 '''
 
+def findMax(lst):
+    if len(lst) ==0:
+        return None
+    max = lst[0]
+    for number in lst[1::]:
+        if max < number:
+            max = number
+    return max
+
+
+
+
 if __name__ =='__main__':
-    lst = [1,2,3,4,5,6]
-    largest_number = max(lst)
+    lst = [1000,1,2,3,4,5,6,99,0,45,100,765]
+    largest_number = max(lst) # built-in function
     print(largest_number)
+    largest_number = findMax(lst)
+    print(largest_number)
+    largest_number = findMax([])
+    print(largest_number)
+    largest_number = findMax([10])
+    print(largest_number)
+
+
+
+
+
