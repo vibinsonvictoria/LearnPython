22e6b5a34f6002f3cab1f012227f3858c1dc7837
Vibinson <vibinvictoria@gmail.com>
2022-09-23 15:46:13 +0530

    Initial Version

---
+++ com/python/intermediate/NumberGuessing.py
@@ -0,0 +1,23 @@
+'''
+    Read input from the user.
+    if the number is matched with secret number then print 'Won the game'
+    otherwise continue the process.
+
+'''
+
+if __name__ =='__main__':
+    secret_number = 12
+    while True:
+        try:
+
+            user_response = int(input('Enter a Number :'))
+        except:
+            print(f'oops.. Required Integer Type')
+            continue
+        else:
+            if user_response == secret_number:
+                print('You Won the game :)')
+                break
+            else:
+                print('Not matched.. Try again..')
+                continue
