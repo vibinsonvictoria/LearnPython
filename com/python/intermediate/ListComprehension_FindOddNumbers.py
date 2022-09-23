84792b17b0548945f309e367d3500c98033bbc64
Vibinson <vibinvictoria@gmail.com>
2022-09-23 15:55:59 +0530

    initial version

---
+++ com/python/intermediate/ListComprehension_FindOddNumbers.py
@@ -0,0 +1,11 @@
+'''
+
+    find odd number from the list using List comprehension
+
+'''
+
+if __name__ == '__main__':
+   lst = list(range(1,11))
+   print(f'The given list :{lst}')
+   odd_numbers = [number for number in lst if not number%2==0]
+   print(f'The odd numbers are {odd_numbers}')
