78c82d2dd9e2483643df85df1470965382ef3aa5
Vibinson <vibinvictoria@gmail.com>
2022-09-23 14:44:11 +0530

    Added HelloWord

---
+++ com/python/basics/FindLargesNumber.py
@@ -0,0 +1,9 @@
+'''
+    Find Largest Number from Given List
+    [1,2,3,4,5,6] => 6
+'''
+
+if __name__ =='__main__':
+    lst = [1,2,3,4,5,6]
+    largest_number = max(lst)
+    print(largest_number)
