64bfbf8c57a117c87ac018c244ad72d6402c3eb7
Vibinson <vibinvictoria@gmail.com>
2022-10-02 13:52:10 +0530

    Added remove value

--- com/set/delete_element_using_remove.py
+++ com/set/delete_element_using_remove.py
@@ -4,4 +4,6 @@ if __name__ == '__main__':
     print(f'original set : {s}') # removed duplicate automatically
     s.remove(2)
     print(f'After remove : {s}')
+    #s.remove(100) # it will throw an error if value not found in set. use discard. refer discard
+
     
